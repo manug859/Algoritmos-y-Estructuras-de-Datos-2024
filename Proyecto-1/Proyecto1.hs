1e44647b61e8a33bab9dcd3036828f5eb3c9006b
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-08-18 15:19:52 -0300

    ejercicios varios

--- Proyecto-1/Proyecto1.hs
+++ Proyecto-1/Proyecto1.hs
@@ -90,26 +90,26 @@ promedio xs = div (sumatoria xs) (length xs)
 --3.a)[4b] La variable libre es "x" siendo esta del tipo Int
 --3.a)[4d] La variable libre es la lista de [Int].
 
---3.b)[4a] ⟨ ∀ i : 0 ≤ i < #xs : xs.i > 0 ⟩
+--[3.b]
 
+--3.b)[4a] ⟨ ∀ i : 0 ≤ i < #xs : xs.i > 0 ⟩
 mayorQueCero :: [Int] -> Bool
 mayorQueCero [] = True
 mayorQueCero (x:xs) = x > 0 && mayorQueCero xs 
 
 
 --3.b)[4b] ⟨ ∃ i : 0 ≤ i < #xs : xs.i = x ⟩
-
 igualAn :: [Int] -> Int -> Bool
 igualAn [] n = False
 igualAn (x:xs) n = x == n || igualAn xs n
 
 
 --3.b)[4d] ⟨ ∀ i : 0 ≤ i < #xs − 1 : xs.i = xs.(i + 1) ⟩
-
 todosIguales :: [Int] -> Bool
 todosIguales [] = True
 todosIguales [x] = False
 todosIguales (y:(x:xs)) = y == x || todosIguales xs
+
 
 --3.c) Evaluá las funciones tomando como argumento los valores señalados en 5.
 
@@ -118,12 +118,10 @@ todosIguales (y:(x:xs)) = y == x || todosIguales xs
 --mayorQueCero [11, 2, 5, 8]
 --True
 
-
 --igualAn [-5, -3, 4, 8] 4
 --True
 --igualAn [11, 2, 5, 8] 4
 --False
-
 
 --todosIguales [1,1,1,1]
 --True
@@ -132,3 +130,30 @@ todosIguales (y:(x:xs)) = y == x || todosIguales xs
 --todosIguales [11, 2, 5, 8]
 --False
 
+--Ejercicio 4
+
+--4.a) "Identifica las variables libres de cada expresión y el tipo de cada una."
+
+--4.a)[7a] La variable libre es n siendo esta del tipo Int.
+--4.a)[7b] La variable libre es xs siendo esta del tipo [Int].
+--4.a)[7c] La variables libres son xs e ys siendo estas del tipo [Int].
+--4.a)[7d] La variable libre es n siendo esta del tipo Int.
+
+--[4.b]
+
+factorial'' :: Int -> Int
+factorial'' 0 = 1
+factorial'' n = n * factorial'' (n-1)
+
+promedio' :: [Int] -> Int
+promedio' [] = 0
+promedio' xs = div (sumatoria xs) (length xs)
+
+--función auxiliar minMayorQueMax
+maxLista :: [Int] -> Int
+maxLista [] = minBound
+maxLista (y:(x:xs)) = max y x  
+
+minMayorQueMax :: [Int] -> [Int] -> Bool
+minMayorQueMax [] [] = True
+minMayorQueMax xs ys = 
