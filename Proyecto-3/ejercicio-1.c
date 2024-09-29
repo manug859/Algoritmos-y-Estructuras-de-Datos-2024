273a1d37634816e8309d54b8a6cc6a1b8d43d261
manug859 <manu.gomez859@gmail.com>
2024-09-29 02:10:29 -0300

    Agregar funciones auxiliares para imprimir valores booleanos y corregir formato de impresión en operaciones matemáticas

--- Proyecto-3/ejercicio-1.c
+++ Proyecto-3/ejercicio-1.c
@@ -11,29 +11,39 @@ int pedir_enteros (char var) {
 }
 
 int operaciones (int x, int y, int z) {
-    printf("x + y + 1 = %d\n", x+y+1);
+    printf("x + y + 1 = %d\n", x + y + 1);
 
     printf("z * z + y * 45 - 15 * x = %d\n", z * z + y * 45 - 15 * x);
 
-    b = y - 2 == (x * 3 + 1) % 5;
-
-    printf("b = %d\n", b);
-
-    printf("y / 2 * x = %d\n", y / 2 * x);
+    // Comparaciones booleanas
+    b = (y - 2 == (x * 3 + 1) % 5);
     
-    w = y < x * z;
+    printf("y / 2 * x = %d\n", y / 2 * x);
 
-    printf("w = %d\n", w);
+    w = (y < x * z);
+
     return 0;
+}
+
+void imprimir_booleano(bool a) {
+    if (a) {
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
 }
 
 int main (void) {
     int x = pedir_enteros('x');
     int y = pedir_enteros('y');
     int z = pedir_enteros('z');
-
-    int res = operaciones(x, y, z);
-    printf(res);
+    operaciones(x, y, z);
+    
+    printf("b = ");
+    imprimir_booleano(b);
+    printf("w = ");
+    imprimir_booleano(w);
+    
 
 return 0;
 }
