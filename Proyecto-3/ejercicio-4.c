0025d5470d75faaf0fa78ee15b92eb119d72c8d9
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-10-03 18:07:40 -0300

    Agregar archivo y función para pedir un entero y realizar una iteración

---
+++ Proyecto-3/ejercicio-4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int pedir_enteros (char var) {
+    int x;
+    printf("Ingrese un valor para %c", var);
+    scanf("%d", &x);
+    return x;    
+}
+
+int iteracion (int i) {
+    while(i != 0) {
+        printf("i = %d\n", i);
+        i--;
+    }
+}
+
+int main (void) {
+    int i = pedir_enteros('i');
+    iteracion(i);
+    return 0;
+}
