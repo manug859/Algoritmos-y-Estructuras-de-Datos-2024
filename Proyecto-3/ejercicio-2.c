346ce7f43b3e5dcef71cf978c3b5c8331ec2003f
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-10-03 17:17:41 -0300

    Agregar función para pedir un entero y realizar una operación condicional

--- Proyecto-3/ejercicio-2.c
+++ Proyecto-3/ejercicio-2.c
@@ -3,17 +3,23 @@
 int pedir_entero (char var) {
     int x;
     printf("Ingrese un valor para %c = ", var);
-    scanf("%d",&x);
-return x;
+    scanf("%d", &x);
+    return x;
 }
 
 int operacion (int x, int y) {
-    if(x>=y)
-    {
+    if(x >= y) {
         x = 0;
-    }
-    else
-    {
+    } else {
         x = 2;
     }
+    return x;
+}
+
+int main (void) {
+    int x = pedir_entero('x');
+    int y = pedir_entero('y');
+    int res = operacion(x, y);
+    printf("Resultado de operacion: x = %d\n", res); // Imprimir el resultado
+    return 0;
 }
