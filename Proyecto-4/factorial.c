7a70578acf41083caac5b3026640614427e8db09
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-10-21 17:36:22 -0300

    Refactorizar factorial.c para agregar función pedir_entero y calcular el factorial de un número

---
+++ Proyecto-4/factorial.c
@@ -0,0 +1,36 @@
+/*
+Const N : Int;
+Var res, n : Int;
+{P : N ≥ 0}
+res, n := 1, 0
+	do n ≠ N → 
+		res, n := res * (n+1), n+1
+	od
+{Q : res = N!}
+*/
+
+int pedir_entero(char name) {
+    int x;
+    printf("Ingrese un valor para %c = ", name);
+    scanf("%d", &x);
+    return x;
+}
+
+int factorial(int x) {
+    int res = 1;
+    int n = 0;
+    while (n != x) {
+        res = res * (n + 1);
+        n = n + 1;
+    }
+    return res;
+}
+
+int main(void) {
+    int x = pedir_entero('x');
+    
+    int res = factorial(x);
+
+    printf("El factorial de x = %d", res);
+    return 0;
+}
