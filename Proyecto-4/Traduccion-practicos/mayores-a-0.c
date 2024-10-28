15a0889df729c69fbde410c1226171f659104e4b
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-10-28 19:57:02 -0300

    Refactorizar código para agregar programa "mayores-a-0.c" que solicita e imprime un arreglo de tamaño constante y verifica si todos los elementos son mayores a 0.

---
+++ Proyecto-4/Traduccion-practicos/mayores-a-0.c
@@ -0,0 +1,49 @@
+/*
+Const N : Int, A : array [0, N) of Int;
+Var pos : Int;
+      res  : Bool;
+{P : N ≥ 0}
+res, pos := True, 0;
+	do pos < N →
+	res, pos := res ∧ A.pos > 0, pos+1
+	od
+{ Q : res = ⟨ ∀ i : 0 ≤ i < N : A.i > 0 ⟩ }
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define N 5
+
+void pedir_arreglo (int tam, int a[]) {
+    int i = 0;
+        while (i < tam) {
+            printf("Ingrese un valor para el indice a[%d] = ", i);
+            scanf("%d", &a[i]);
+            i++;
+        }
+}
+
+bool mayores_a_cero (int tam, int a[]) {
+    int pos = 0;
+    bool res = true;
+    while (pos < tam) {
+        res = res && a[pos] > 0;
+        pos = pos + 1;
+    }
+    return res;
+}
+
+int main(void) {
+    int a[N];
+    pedir_arreglo(N, a);
+    bool res = mayores_a_cero(N, a);
+    
+    if (res) {
+        printf("Todos los elementos del arreglo son mayores a 0");
+    } else {
+        printf("No todos los elementos del arreglo son mayores a 0");
+    }
+
+    return 0;
+}
