269d5dbbbfd3e4bc27b7734eb3980062bf9006a8
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-11-03 17:27:23 -0300

    Agregar función 'multiplo_minimo' y función 'pedir_arreglo' en 'ej2-2022-08-31.c' para calcular el múltiplo mínimo de un arreglo.

--- Practicas-parciales/parciales_c/ej2-2022-08-31.c
+++ Practicas-parciales/parciales_c/ej2-2022-08-31.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
 #include <assert.h>
+#include <limits.h>
+
+#define N 5
+
+void pedir_arreglo (int tam, int a[]) {
+    int i = 0;
+    while(i < tam) {
+        printf("Ingrese un valor para el indice a[%d] = ", i);
+        scanf("%d", &a[i]);
+        i++;
+    }
+}
 
 int pedir_entero (char name) {
     int x;
-    printf("Ingrese el valor de %c: ", name);
+    printf("Ingrese un valor para %c = ", name);
     scanf("%d", &x);
     return x;
 }
 
-
+int multiplo_minimo (int a[], int tam, int k) {
+    int i = 0;
+    int min_mul = INT_MAX;
+    while (i < tam) {
+        if((a[i]%k) == 0) {
+            if(a[i] < min_mul) {
+                min_mul = a[i];
+            }
+        }
+        i++;
+    }
+    return min_mul;
+}
 
 int main(void) {
+    int a[N];
+
+    pedir_arreglo(N, a);
+
+    int k = pedir_entero('k');
+
+    int res = multiplo_minimo (a, N, k);
+    printf("res = %d", res);
     return 0;
 }
