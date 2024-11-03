398914753868b42a8ccbdd80fbe38bb53a80f949
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-11-03 17:08:00 -0300

    Eliminar la función 'operacion' y su invocación en 'main' en el archivo 'ej2-2022-08-31.c'

--- Practicas-parciales/parciales_c/ej2-2022-08-31.c
+++ Practicas-parciales/parciales_c/ej2-2022-08-31.c
@@ -8,31 +8,8 @@ int pedir_entero (char name) {
     return x;
 }
 
-void operacion (int x, int y, int z) {
-    int X = x;
-    int Y = y;
-    int Z = z;
 
-    assert(X == x && Y == y && Z == z); // Precondicion
-
-    int temp_x = x;
-    int temp_y = y;
-
-    x = (2*temp_y) + temp_x;
-    y = z + temp_x;
-    z = temp_y / temp_x;
-
-    assert(x == 2*Y + X && y == Z + X && z == Y / X); // Postcondicion
-
-    printf("x = %d, y = %d, z = %d\n", x, y, z);
-}
 
 int main(void) {
-    int x = pedir_entero('x');
-    int y = pedir_entero('y');
-    int z = pedir_entero('z');
-
-    operacion(x, y, z);
-
     return 0;
 }
