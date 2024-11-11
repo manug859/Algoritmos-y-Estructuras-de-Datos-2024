b02039091165d97fdbfebe051505eec3ab900aed
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-11-11 02:24:32 -0300

    Agregar implementación de algoritmo para calcular la máxima diferencia en un arreglo

---
+++ Proyecto-4/Traduccion-practicos/maxima-diferencia.c
@@ -0,0 +1,58 @@
+/*
+Const N : Int;
+Var a : array[0, N) of Int, r : Int;
+{P : N ≥ 1}
+  r, s, n := -INF, a.0, 1 
+	do n < N →
+	r, s, n := r max (s - a.n), s max a.n, n+1
+	od
+{Q : r = ⟨ Max p, q : 0 ≤ p < q < N : a.p - a.q ⟩ }
+*/
+
+#include <stdio.h>
+#include <limits.h>
+#include <assert.h>
+
+#define N 5
+
+void pedir_arreglo(int tam, int a[]) {
+    int i = 0;
+        while(i < tam) {
+            printf("Ingrese un valor para el indice a[%d] = ", i);
+            scanf("%d", &a[i]);
+            i++;
+        }
+}
+
+int max (int a, int b) {
+    if(a >= b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+int maxima_dif (int tam, int a[]) {
+    int r = INT_MIN;
+    int s = a[0];
+    int n = 1;
+
+    while (n < tam) {
+        r = max(r, (s - a[n]));
+        s = max(s, a[n]);
+        n++; 
+    }
+    return r;
+}
+
+int main (void) {
+    int a[N];
+
+    pedir_arreglo(N, a);
+
+    int r = maxima_dif(N, a);
+    printf("r = %d ", r);
+
+    return 0;
+}
